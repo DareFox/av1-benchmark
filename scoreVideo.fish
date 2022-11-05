ad22187e92f7a5a80b167229d2f4372c7310fa7d
DareFox <easymalink@gmail.com>
2022-11-05 06:31:33 +0300

    Print VMAF rating message at the beginning

--- scoreVideo.fish
+++ scoreVideo.fish
@@ -3,6 +3,8 @@ set outputTo $argv[1]
 set original $argv[2]
 set distorted $argv[3]
 
+printf "VMAF Rating\n\tOriginal: $original\n\tDistorted: $distorted"
+printf "\nLogs will be written to $outputTo\n"
 set nameDistorted $(basename $distorted)
 
 ffmpeg -i $distorted -i $original -threads 12 -filter_complex libvmaf -f null - 2>&1 | tee $outputTo/$nameDistorted.vmaf.log
