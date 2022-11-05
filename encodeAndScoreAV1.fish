39bbbaef626b27be7c63f61e84086c71dd0c1e52
DareFox <easymalink@gmail.com>
2022-11-05 05:30:16 +0300

    Fix breakline

--- encodeAndScoreAV1.fish
+++ encodeAndScoreAV1.fish
@@ -20,7 +20,6 @@ if not test -d $resultsFolder
 end
 
 set processedFilesList $resultsFolder/processed
-
 set gnuTimeFormat "Time result\nCommand: %C\nExit code: %x\n\nSimplified elapsed real time: %E\nElapsed real time: %es\n\nCPU percentage (user+system/time): %P\nCPU-seconds used in kernel: %S\nCPU-seconds used in user space: %U\n\nMajor or I/O page faults: %F\nMinor or recoverable page faults: %R\n\nFile system inputs: %I\nFile system outputs: %O\n\nLife-time max resident set size: %Mkb\nAvg resident set size: %tkb\nAvg total memory use (data+stack+text): %Kkb\nAvg unshared data area: %Dkb\nAvg unshared stack space: %pkb\nAvg shared text space: %X\n\nSystem's page size: %Z bytes\n\nNumber of swaps of main memory: %W\nNumber of context-switched swaps: %c\nNumber of waits: %w\n\nNumber of socket messages received: %r\nNumber of socket messages sent: %s\nNumber of signals delibered to the process %k"
 
 set presets 6 7 8 9 10
@@ -51,8 +50,7 @@ for preset in $presets
                 trap "echo \nCaught SIGINT! Removing all $basenameExport\* files \(because they are unfinished\).; ./removeFilesByBasename.fish $resultsFolder $basenameExport; exit" SIGINT
 
                 command time -f $gnuTimeFormat ffmpeg -report -i $sample -c:v libsvtav1 -preset $preset -crf $crf \
-                -svtav1-params film-grain=$filmGrain:fast-decode=$fastDecode -map 0:v -map 0:a -c:a libopus -b:a 128k \ 
-                $resultsFolder/$filenameExport 2>&1 | tee $gnuTimeLogFileExport
+                -svtav1-params film-grain=$filmGrain:fast-decode=$fastDecode -map 0:v -map 0:a -c:a libopus -b:a 128k $resultsFolder/$filenameExport 2>&1 | tee $gnuTimeLogFileExport
     
                 # Remove auto clean
                 trap - SIGINT
