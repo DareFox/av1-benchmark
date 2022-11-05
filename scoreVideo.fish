d8d1fbc00a080e30400620698063d2a9401e1f2f
DareFox <easymalink@gmail.com>
2022-11-05 06:31:21 +0300

    Fix "basename: missing opernand" when scoring

--- scoreVideo.fish
+++ scoreVideo.fish
@@ -1,9 +1,8 @@
 #!/bin/env fish
-set original $argv[2]
-
-set distorted $arv[3]
-set nameDistorted (basename $distorted)
-
 set outputTo $argv[1]
+set original $argv[2]
+set distorted $argv[3]
 
-ffmpeg -i $distorted  -i $original -threads 12 -filter_complex libvmaf -f null - 2>&1 | tee $outputTo/$nameDistorted.vmaf.log
+set nameDistorted $(basename $distorted)
+
+ffmpeg -i $distorted -i $original -threads 12 -filter_complex libvmaf -f null - 2>&1 | tee $outputTo/$nameDistorted.vmaf.log
