c0b62a7411675381fba310b1a49d830c2278a544
DareFox <easymalink@gmail.com>
2022-11-05 04:31:10 +0300

    Create results folder

--- encodeAndScoreAV1.fish
+++ encodeAndScoreAV1.fish
@@ -1,3 +1,13 @@
 #!/bin/env fish
 
 set sample $argv[1]
+set basenameSample $(basename $sample)
+set basedirSample $(dirname $sample)
+
+set resultsFolder $basedirSample/$basenameSample-RESULTS
+
+if not test -d $resultsFolder
+    mkdir $resultsFolder
+end
+
+
