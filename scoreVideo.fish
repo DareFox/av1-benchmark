f06f4eb76137951c00fced9dff80ad1089f159cd
DareFox <easymalink@gmail.com>
2022-11-08 02:47:15 +0300

    ScoreVideo: Fix no extension in name

--- scoreVideo.fish
+++ scoreVideo.fish
@@ -3,12 +3,11 @@ set outputTo $argv[1]
 set original $argv[2]
 
 set distorted $argv[3]
-set distortedExtension (path extension $distorted)
 
 printf "VMAF Rating\n\tOriginal: $original\n\tDistorted: $distorted"
 printf "\nLogs will be written to $outputTo\n"
 
-set nameDistorted $(basename $distorted $distortedExtension)
+set nameDistorted $(basename $distorted)
 set logPath $outputTo/$nameDistorted.vmaf.log
 
 set gnuTimeFormat "Time result\nCommand: %C\nExit code: %x\n\nSimplified elapsed real time: %E\nElapsed real time: %es\n\nCPU percentage (user+system/time): %P\nCPU-seconds used in kernel: %S\nCPU-seconds used in user space: %U\n\nMajor or I/O page faults: %F\nMinor or recoverable page faults: %R\n\nFile system inputs: %I\nFile system outputs: %O\n\nLife-time max resident set size: %Mkb\nAvg resident set size: %tkb\nAvg total memory use (data+stack+text): %Kkb\nAvg unshared data area: %Dkb\nAvg unshared stack space: %pkb\nAvg shared text space: %X\n\nSystem's page size: %Z bytes\n\nNumber of swaps of main memory: %W\nNumber of context-switched swaps: %c\nNumber of waits: %w\n\nNumber of socket messages received: %r\nNumber of socket messages sent: %s\nNumber of signals delibered to the process %k"
