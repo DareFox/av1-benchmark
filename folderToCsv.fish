c9f00d4f11ce481ddfea5eca4b93523350b26314
DareFox <easymalink@gmail.com>
2022-11-07 04:36:18 +0300

    Save file size to CSV file

--- folderToCsv.fish
+++ folderToCsv.fish
@@ -8,7 +8,7 @@ if not test -f $folder/$processedFileList
     exit 1
 end
 
-echo "Preset;CRF;Film Grain Level;Is Fast Decode used;VMAF Score;Encode elapsed time in seconds;Sample name" > $csvFile
+echo "Preset;CRF;Film Grain Level;Is Fast Decode used;VMAF Score;Encode elapsed time in seconds;Size in MiB;Sample name" > $csvFile
 
 cat $folder/$processedFileList | while read -l filename
     set encodeTimeSeconds (cat $filename*.gnu-time.log | grep -Po '(?<=Elapsed real time: )\\S*(?=s)')
@@ -17,7 +17,8 @@ cat $folder/$processedFileList | while read -l filename
     set crf (echo $filename | grep -Po '(?<=crf=)[^-]*')   
     set filmGrain (echo $filename | grep -Po '(?<=filmGrain=)[^-]*')   
     set fastDecode (echo $filename | grep -Po '(?<=fastDecode=)[^-]*')   
+    set mibSize (du -s -k $filename.mkv | awk '{printf "%.3f MiB %s\n", $1/1024, $2}' | grep -Po '.*(?=MiB)')
     
-    echo "$preset;$crf;$filmGrain;$fastDecode;$vmafScore;$encodeTimeSeconds;$filename" >> $csvFile
+    echo "$preset;$crf;$filmGrain;$fastDecode;$vmafScore;$encodeTimeSeconds;$mibSize;$filename" >> $csvFile
     echo "Processed $filename"
 end
