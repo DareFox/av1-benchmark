443e20c45d7fdead2812ed7d6d7600aa6ac2ca4a
DareFox <easymalink@gmail.com>
2022-11-08 07:57:12 +0300

    FolderToCSV: Format decimals with comma
    Fuck locales, all my homies hate locales.
    Also fuck Google Sheet automatic formating, which doesn't recognise 0.2 as decimal, but with comma it does

--- folderToCsv.fish
+++ folderToCsv.fish
@@ -13,13 +13,13 @@ echo "Preset;CRF;Film Grain Level;Is Fast Decode used;VMAF Score;Encode elapsed 
 cat $folder/$processedFileList | while read -l fileBasename
     set filename $folder/$fileBasename
 
-    set encodeTimeSeconds (cat $filename*.gnu-time.log | grep -Po '(?<=Elapsed real time: )\\S*(?=s)')
-    set vmafScore (cat $filename.vmaf.log | grep -Po '(?<=VMAF score: )\\S*') 
+    set encodeTimeSeconds (cat $filename*.gnu-time.log | grep -Po '(?<=Elapsed real time: )\\S*(?=s)' | sed 's/\./,/')
+    set vmafScore (cat $filename.vmaf.log | grep -Po '(?<=VMAF score: )\\S*' | sed 's/\./,/') 
     set preset (echo $filename | grep -Po '(?<=preset=)\d*')
     set crf (echo $filename | grep -Po '(?<=crf=)\d*')   
     set filmGrain (echo $filename | grep -Po '(?<=filmGrain=)\d*')   
     set fastDecode (echo $filename | grep -Po '(?<=fastDecode=)\d*')   
-    set mibSize (du -s -k $filename | awk '{printf "%.3f MiB %s\n", $1/1024, $2}' | grep -Po '.*(?=MiB)')
+    set mibSize (du -s -k $filename | awk '{printf "%.3f MiB %s\n", $1/1024, $2}' | grep -Po '.*(?=MiB)' | sed 's/\./,/')
     
     echo "$preset;$crf;$filmGrain;$fastDecode;$vmafScore;$encodeTimeSeconds;$mibSize;$fileBasename" >> $csvFile
     echo "Processed $fileBasename"
